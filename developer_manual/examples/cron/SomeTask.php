42416cf56c9f0c8d01c31c2d8ce5956d2a394859
Matthew Setter <matthew@matthewsetter.com>
2017-01-10 17:11:53 +0100

    Update SomeTask to match the ownCloud coding style

--- developer_manual/examples/cron/SomeTask.php
+++ developer_manual/examples/cron/SomeTask.php
@@ -4,10 +4,9 @@ namespace OCA\MyApp\Cron;
 
 use \OCA\MyApp\AppInfo\Application;
 
-class SomeTask extends OC\BackgroundJob\TimedJob
-{
-    public static function run()
-    {
+class SomeTask extends OC\BackgroundJob\TimedJob {
+    
+    protected function run($argument) {
         (new Application())
             ->getContainer()
             ->query('SomeService')
