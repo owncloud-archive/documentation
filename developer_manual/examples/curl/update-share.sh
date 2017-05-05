a8914b8c3a1ca00be47b222f820e203fa356c442
Matthew Setter <matthew@matthewsetter.com>
2017-05-05 11:28:42 +0200

    url encode the link name examples in create and update share examples

--- developer_manual/examples/curl/update-share.sh
+++ developer_manual/examples/curl/update-share.sh
@@ -8,5 +8,5 @@ API_PATH=ocs/v1.php/apps/files_sharing/api/v1
 
 curl --user your.username:your.password "$SERVER_URI/$API_PATH/shares/115470" \
      --request PUT \
-     --data 'expireDate=2017-01-02&name=paris photo'
+     --data 'expireDate=2017-01-02&name=paris%20photo'
 
