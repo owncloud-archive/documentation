bfa54e5662863b3502b014abb557f31fe7805b7a
Thomas Boerger <thomas@webhippie.de>
2017-05-17 14:15:03 +0200

    Use proper variable name within go.php and check param

--- go.php
+++ go.php
@@ -16,7 +16,7 @@ $mapping = array(
     'admin-source_install'    => '/admin_manual/installation/source_installation.html',
     'admin-backup'            => '/admin_manual/maintenance/backup.html',
     'admin-monitoring'        => '/admin_manual/operations/considerations_on_monitoring.html',
-    
+
     'admin-performance'       => '/admin_manual/configuration_server/oc_server_tuning.html',
     'admin-config'            => '/admin_manual/configuration_server/config_sample_php_parameters.html',
     'admin-db-conversion'     => '/admin_manual/configuration_database/db_conversion.html',
@@ -25,7 +25,7 @@ $mapping = array(
     'admin-reverse-proxy'     => '/admin_manual/configuration_server/reverse_proxy_configuration.html',
     'admin-php-fpm'           => '/admin_manual/installation/source_installation.html#configuration-notes-to-php-fpm',
     'admin-transactional-locking' => '/admin_manual/configuration_files/files_locking_transactional.html',
-    
+
     'admin-enterprise-license' => '/admin_manual/enterprise_installation/license_keys_installation.html',
 
     'developer-theming'       => '/developer_manual/core/theming.html',
@@ -43,13 +43,11 @@ $mapping = array(
 
 ############# Do not edit below this line #################
 
-$from = $_GET['to'];
+$from = isset($_GET['to']) ? $_GET['to'] : '';
 $proto = isset($_SERVER['HTTPS']) ? 'https' : 'http';
-$port = $_SERVER['SERVER_PORT'];
-$port = ($port !== '80' && $port !== '443') ? ":$port" : '';
-$name = $_SERVER['SERVER_NAME'];
+$name = $_SERVER['HTTP_HOST'];
 $path = dirname($_SERVER['REQUEST_URI']);
-$location = "$proto://$name$port$path";
+$location = "$proto://$name$path";
 
 header('HTTP/1.1 302 Moved Temporarily');
 if (array_key_exists($from, $mapping)) {
@@ -63,4 +61,3 @@ if (array_key_exists($from, $mapping)) {
         header('Location: ' . $location . '/user_manual');
     }
 }
-
