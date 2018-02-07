20b27d220cddb837b5f2042272e0bbdf8043f8d3
Vincent Petry <pvince81@owncloud.com>
2018-02-07 10:31:55 +0100

    Set versions to 10.0.7

--- user_manual/conf.py
+++ user_manual/conf.py
@@ -44,9 +44,9 @@ master_doc = 'contents'
 # built documents.
 #
 # The short X.Y version.
-version = '10.0.6'
+version = '10.0.7'
 # The full version, including alpha/beta/rc tags.
-release = '10.0.6'
+release = '10.0.7'
 
 # General information about the project.
 project = u'ownCloud %s User Manual' % (version)
