95e91694faa96afb684e8f508e7fefe9e6eeaaf7
Matthew Setter <matthew@matthewsetter.com>
2018-03-28 13:37:56 +0200

    Revise the order of the Let's Encrypt documentation

--- admin_manual/installation/examples/lets_encrypt/delete.sh
+++ admin_manual/installation/examples/lets_encrypt/delete.sh
@@ -3,6 +3,19 @@
 LE_PATH="/opt/letsencrypt"
 LE_CB="certbot"
 
+##
+## Retrieve and print a list of the installed Let's Encrypt SSL certificates.
+##
+function get_certificate_names()
+{
+  "$LE_PATH/$LE_CB" certificates | grep -iE "certificate name" | awk -F: '{gsub(/\s+/, "", $2); printf("- %s\n", $2)}'
+}
+
+echo "Available Certificates:"
+
+get_certificate_names
+echo
+
 read -p "Which certificate do you want to delete: " -r -e answer
 if [ -n "$answer" ]; then
   "$LE_PATH/$LE_CB" delete --cert-name "$answer"
