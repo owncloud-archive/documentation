fc6e68b4b090187a2c2d04b01711380a156c7837
Matthew Setter <matthew@matthewsetter.com>
2018-07-20 17:53:01 +0200

    Refactor in an OOP-style
    The previous script, while ok, wasn't defensive enough. So this commit
    rewrites it in an OOP style with much more checking in place. Yes, it
    could have been done as it was, but I prefer this style.

--- developer_manual/scripts/read-config.php
+++ developer_manual/scripts/read-config.php
@@ -19,6 +19,63 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
+
+/**
+ * Class SimpleConfigReader
+ * @package ConfigReader
+ */
+class SimpleConfigReader
+{
+	/**
+	 * @var array
+	 */
+	private $config = [];
+
+	/**
+	 * String returned to the user.
+	 * @var string
+	 */
+	private $output = 'No writable apps directory was found.';
+
+	/**
+	 * SimpleConfigReader constructor.
+	 * @param string $config
+	 */
+	public function __construct($config = '')
+	{
+		$this->config = $config;
+	}
+
+	/**
+	 * Find the first app directory path that is set as
+	 * being writable and is physically writable in the filesystem
+	 *
+	 * @return string
+	 * @throws \Exception
+	 */
+	function findPath()
+	{
+		if (!array_key_exists('apps_paths', $this->config)) {
+			throw new \Exception('Configuration is missing the apps_path key');
+		}
+
+		foreach ($this->config['apps_paths'] as $path) {
+			if ($path['writable'] == true && is_writable($path['path'])) {
+				$this->output = $path['path'];
+				break;
+			}
+		}
+
+		return $this->output;
+	}
+}
+
+/*
+ * As per the PHP manual: The first argument $argv[0] is always the name that
+ * was used to run the script. So we need at least two to access the new app's
+ * name, as well as the running script's name.
+ * @see https://secure.php.net/manual/en/reserved.variables.argv.php
+ */
 if (count($argv) != 2) {
     echo "Need to provide the path to the ownCloud directory.";
     exit(1);
@@ -28,19 +85,5 @@ $ownCloudConfigFile = sprintf("%s/config/config.php", (string) $argv[1]);
 
 if (file_exists($ownCloudConfigFile)) {
     require_once($ownCloudConfigFile);
-    $output = "No writable apps directory was found.";
-
-    if (array_key_exists('apps_paths', $CONFIG)) {
-        // Returns the first available app directory, if one is available.
-        foreach ($CONFIG['apps_paths'] as $path) {
-            if ($path['writable'] == true) {
-                $output = $path['path'];
-                break;
-            }
-        }
-    }
-
-    echo $output;
-} else {
-    print "Could not find the specified config file: $ownCloudConfigFile.";
+    print (new SimpleConfigReader($CONFIG))->findPath();
 }
